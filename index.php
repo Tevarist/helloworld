db4ea4e980d14af3525dd4c501074d8ccdd54afe
Tevarist <34331225+Tevarist@users.noreply.github.com>
2018-09-21 09:07:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>my VCS </title>
+<style type="css/stylesheet>
+#hello{
+color:blue;
+font-size: 200%;
+}
+</style>
+</head>
+<body>
+<div id="hello"> Hello VCS</div>
+
+</body>
+</html>
